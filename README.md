70a25c1abf9788954a764abc22e836b4d72984f3
Yannick van Diermen <yannick@Yannicks-MacBook-Pro.local>
2020-04-24 22:52:23 +0200

    toevoegingen voor herkansers

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # ai-project
 
+Maak ten eerste een bestand aan met de naam '.env'.
+Zet daar dan de volgende code in:
+```
+DBNAME= <naam van de database>
+DBUSER= <gebruikersnaam>
+DBPASSWORD= <wachtwoord gebruiker>
+```
+
 De testomgeving is te vinden in de map client-code.
 De recommendation engine is te vinden in api.py.
+
+Voeg de csv bestanden toe aan de map database-setup.
+Run daarna export.py om de csv bestanden in te lezen in je database.
---
+++ database-setup/export.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv, find_dotenv
+import psycopg2
+
+def export_csv(db, filename, tablename):
+    cur = db.cursor()
+
+    f = open(filename, 'r')
+
+    copy = f""" COPY {tablename} FROM stdin WITH CSV HEADER
+            DELIMITER as ','"""
+
+    cur.copy_expert(sql=copy, file=f)
+
+    db.commit()
+    cur.close()
+
+def export_products(db):
+    export_csv(db, 'products.csv', 'products')
+
+def export_profiles(db):
+    export_csv(db, 'profiles.csv', 'profiles')
+
+def export_orders(db):
+    export_csv(db, 'orders.csv', 'orders(session_id, product_id)')
+
+def export_sessions(db):
+    export_csv(db, 'sessions.csv', 'sessions')
+
+def export_recommended(db):
+    export_csv(db, 'recommended.csv', 'recommended(profile_id, product_id)')
+
+
+load_dotenv(find_dotenv())
+
+db = psycopg2.connect(dbname=os.getenv("DBNAME"), user=os.getenv("DBUSER"), password=os.getenv("DBPASSWORD"))
+
+export_orders(db)
+export_profiles(db)
+export_orders(db)
+export_sessions(db)
+export_recommended(db)
+
+db.close()
